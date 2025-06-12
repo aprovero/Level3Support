65ae57c464a59181fb578ad9b94b0787a02f14e7
aprovero <aprovero@gmail.com>
2025-06-12 17:59:10 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -119,8 +119,6 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
-});
-
 
 // =======================
 // 💬 MESSAGE HANDLER
