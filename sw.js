642c01c25e09daaaa9560405d3b5af662926deb5
Andres Provero <aprovero@gmail.com>
2026-05-26 00:20:59 -0600

    style: fix vertical alignment offset on search bar, compress wrapper padding and dropdown items to v1.2.5

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // =======================
 // 🔁 CACHE VERSIONING
 // =======================
-const CACHE_NAME = 'level3support-cache-v18';
+const CACHE_NAME = 'level3support-cache-v19';
 
 const ASSETS_TO_CACHE = [
   '/index.html', '/404.html', '/offline.html',
