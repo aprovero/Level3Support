26ba2864d25cba611200f734921b59427a370805
Andres Provero <aprovero@gmail.com>
2026-05-26 20:13:50 -0600

    style: restructure relay configurator inputs for vertical SW layout and perfect text alignment to v2.1.3

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // =======================
 // 🔁 CACHE VERSIONING
 // =======================
-const CACHE_NAME = 'level3support-cache-v22';
+const CACHE_NAME = 'level3support-cache-v23';
 
 const ASSETS_TO_CACHE = [
   '/index.html', '/404.html', '/offline.html',
